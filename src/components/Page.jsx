022060714354eb7198062856cd7d6bb4a3cfb3ac
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-01 22:38:07 +0300

    ESLint error correction

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Page extends Component {
-
   onBtnClick = (e) => {
     const year = +e.currentTarget.innerText;
+    // eslint-disable-next-line react/destructuring-assignment
     this.props.setYear(year);
   }
 
@@ -13,24 +13,33 @@ class Page extends Component {
     return (
       <div>
         <div>
-          <button onClick={this.onBtnClick}>2020</button>
-          <button onClick={this.onBtnClick}>2019</button>
-          <button onClick={this.onBtnClick}>2018</button>
-          <button onClick={this.onBtnClick}>2017</button>
-          <button onClick={this.onBtnClick}>2016</button>
+          <button type="button" onClick={this.onBtnClick}>2020</button>
+          <button type="button" onClick={this.onBtnClick}>2019</button>
+          <button type="button" onClick={this.onBtnClick}>2018</button>
+          <button type="button" onClick={this.onBtnClick}>2017</button>
+          <button type="button" onClick={this.onBtnClick}>2016</button>
         </div>
         <p>
-          У тебя {photos.length} фото за {year} год
+          У тебя
+          {' '}
+          {photos.length}
+          {' '}
+          фото за
+          {' '}
+          {year}
+          {' '}
+          год
         </p>
       </div>
-    )
+    );
   }
 }
 
 Page.propTypes = {
   year: PropTypes.number.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
   photos: PropTypes.array.isRequired,
   setYear: PropTypes.func.isRequired,
-}
+};
 
 export default Page;
