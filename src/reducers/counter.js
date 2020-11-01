970f54ae6780da8de67600827abfa0c0b433d444
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-01 23:41:33 +0300

    Создал константу для action

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,10 +1,12 @@
+import { INCREASE_COUNTER } from '../actions/PageActions';
+
 const initialState = {
   counter: 0,
 };
 
 function counterReducer(state = initialState, action) {
   switch (action.type) {
-    case 'INCREASE_COUNTER':
+    case INCREASE_COUNTER:
       return { ...state, counter: action.payload + 1 };
 
     default:
