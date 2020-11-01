16e7698f864d1b253329e4b0e893448fa93023a7
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-01 17:57:52 +0300

    Изменил логику компонента Page

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class Page extends Component {
+
+  onBtnClick = (e) => {
+    const year = +e.currentTarget.innerText;
+    this.props.setYear(year);
+  }
+
   render() {
     const { year, photos } = this.props;
     return (
       <div>
+        <div>
+          <button onClick={this.onBtnClick}>2020</button>
+          <button onClick={this.onBtnClick}>2019</button>
+          <button onClick={this.onBtnClick}>2018</button>
+          <button onClick={this.onBtnClick}>2017</button>
+          <button onClick={this.onBtnClick}>2016</button>
+        </div>
         <p>
           У тебя {photos.length} фото за {year} год
         </p>
@@ -17,4 +30,5 @@ export class Page extends Component {
 Page.propTypes = {
   year: PropTypes.number.isRequired,
   photos: PropTypes.array.isRequired,
+  setYear: PropTypes.func.isRequired,
 }
