d0445f45c6489bb2555ac2cab658b00038cfa210
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-01 23:41:36 +0300

    Создал константу для action

--- src/reducers/page.js
+++ src/reducers/page.js
@@ -1,3 +1,5 @@
+import { SET_YEAR } from '../actions/PageActions';
+
 const initialState = {
   year: 2020,
   photos: [],
@@ -5,7 +7,7 @@ const initialState = {
 
 function pageReducer(state = initialState, action) {
   switch (action.type) {
-    case 'SET_YEAR':
+    case SET_YEAR:
       return { ...state, year: action.payload };
 
     default:
