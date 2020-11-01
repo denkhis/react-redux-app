68cab2d00146866fc44e003e40106b4536cb1c19
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-02 02:00:47 +0300

    Удалил лишний компонент

--- src/actions/PageActions.js
+++ src/actions/PageActions.js
@@ -1,16 +1,8 @@
 export const SET_YEAR = 'SET_YEAR';
-export const INCREASE_COUNTER = 'INCREASE_COUNTER';
 
 export function setYear(year) {
   return {
     type: SET_YEAR,
     payload: year,
-  };
-}
-
-export function increaseCounter(counter) {
-  return {
-    type: INCREASE_COUNTER,
-    payload: counter,
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 // eslint-disable-next-line import/named
 import pageReducer from './page';
 import userReducer from './user';
-import counterReducer from './counter';
 
 const rootReducer = combineReducers({
   page: pageReducer,
   user: userReducer,
-  counter: counterReducer,
 });
 
 export default rootReducer;
