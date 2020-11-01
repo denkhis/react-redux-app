2fc8382ab95f8e593bf3b878110e99ea2e74b108
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-01 20:17:00 +0300

    Добавил компонент counter

---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Counter extends Component {
+
+  onCounterClick = () => {
+    this.props.increaseCounter(this.props.counter);
+  }
+
+  render() {
+    const { counter } = this.props;
+    return (
+      <div>
+        <button onClick={this.onCounterClick}>
+          Увеличить
+        </button>
+        <p>
+          Счетчик: {counter}
+        </p>
+      </div>
+    )
+  }
+}
+
+Counter.propTypes = {
+  counter: PropTypes.number.isRequired,
+  increaseCounter: PropTypes.func.isRequired,
+}
