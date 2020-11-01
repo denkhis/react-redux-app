00b31d4f1fea22b975111feeda4df78c57f0a512
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-01 20:15:48 +0300

    Изменение в app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Page } from '../components/Page';
 import { User } from '../components/User';
+import { Counter } from '../components/Counter';
 import { setYear} from '../actions/PageActions';
+import { increaseCounter} from '../actions/PageActions';
 
 import './App.css';
 
 class App extends Component {
   render() {
-    const { user, page, setYearAction } = this.props;
+    const { user, page, counter, setYearAction, increaseCounterAction } = this.props;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Мой топ фото</h1>
         </header>
         <User name={user.name} />
-        <Page photos={page.photos}  year={page.year} setYear={setYearAction}/>
+        <Page photos={page.photos} year={page.year} setYear={setYearAction}/>
+        <Counter counter={counter} increaseCounter={increaseCounterAction}/>
       </div>
     )
   }
@@ -26,12 +29,14 @@ const mapStateToProps = (store) => {
   return {
     user: store.user,
     page: store.page,
+    counter: store.counter,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    setYearAction: (year) => dispatch(setYear(year))
+    setYearAction: (year) => dispatch(setYear(year)),
+    increaseCounterAction: (counter) => dispatch(increaseCounter(counter)),
   }
 }
 
