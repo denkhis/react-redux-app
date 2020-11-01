9d0025983aa7d2e8a46410966d2238a8d1d5c7db
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-01 22:38:16 +0300

    ESLint error correction

--- src/actions/PageActions.js
+++ src/actions/PageActions.js
@@ -2,12 +2,12 @@ export function setYear(year) {
   return {
     type: 'SET_YEAR',
     payload: year,
-  }
+  };
 }
 
 export function increaseCounter(counter) {
   return {
     type: 'INCREASE_COUNTER',
     payload: counter,
-  }
+  };
 }
