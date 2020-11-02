d92ee132f1479cc751438438b03baee17581f042
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-02 16:16:38 +0300

    Изменил конфигурацию Store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
