ed8d2a4865ae62b390903acb51f3244f958653ce
Denis Khisamiev <dnkhisamiev@gmail.com>
2020-11-02 16:17:06 +0300

    изменил логику корневого компонента

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,18 +3,23 @@ import { connect } from 'react-redux';
 import Page from '../components/Page';
 import User from '../components/User';
 // eslint-disable-next-line import/no-duplicates
-import { setYear } from '../actions/PageActions';
+import { getPhotos } from '../actions/PageActions';
 // eslint-disable-next-line import/no-duplicates
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     const {
-      user, page, setYearAction,
+      user, page, getPhotosAction,
     } = this.props;
     return (
       <div className="row">
-        <Page photos={page.photos} year={page.year} setYear={setYearAction} />
+        <Page
+          photos={page.photos}
+          year={page.year}
+          isFetching={page.isFetching}
+          getPhotos={getPhotosAction}
+        />
         <User name={user.name} />
       </div>
     );
@@ -28,7 +33,8 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  setYearAction: (year) => dispatch(setYear(year)),
+  // диспатчим санку
+  getPhotosAction: (year) => dispatch(getPhotos(year)),
 });
 
 export default connect(
